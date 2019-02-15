b9a3963a469d7378ed86e43547843d57e54df49e
FannarHrafn <fannarhrafn@gmail.com>
2019-02-15 14:50:33 +0000

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,4 @@
+
 def fall(L):
     haesta = max(L)
     countL = [0]*(haesta+1)
@@ -29,3 +30,93 @@ def linearsearch(list,search):
             return x
 
 print linearsearch(linearlist,7)
+
+
+binarylist = [1,2,3,5,6,7,8,9]
+"""
+print "binary"
+def binarysearch(listi,target):
+    middle = listi[len(listi)//2]
+    print listi
+    print middle
+
+    if middle == target:
+        return "index: " + str(listi[len(listi)/2]-1)
+    elif middle > target:
+        return binarysearch(listi[:middle-1],target)
+    elif middle < target:
+        return binarysearch(listi[middle-1:],target)
+"""
+#print(binarysearch(binarylist,7))
+def binarysearch(listi,l,h,target):
+    if l<h:
+        middle = (l + (h-1))/2
+        print listi
+        print middle
+
+        if listi[middle] == target:
+            return middle
+        elif listi[middle] > target:
+            return binarysearch(listi,l,middle-1,target)
+        elif listi[middle] < target:
+            return binarysearch(listi,middle+1,h,target)
+    else:
+        return -1
+
+print(binarysearch(binarylist,0,len(binarylist),7))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#geyma tetta til seinna
+'''
+class Node:
+    def __init__(self,v):
+        self.value = v
+        self.left = None
+        self.right = None
+
+    def insert(self,d):
+        if self.value == d:
+            return False
+        elif self.value > d:
+            if self.left:
+                return self.left.insert(d)
+            else:
+                self.left = Node(d)
+                return True
+        else:
+            if self.right:
+                return self.right.insert(d)
+            else:
+                self.right = Node(d)
+                return True
+class Tree:
+    def __index__(self):
+        self.root = None
+
+    def insert(self,d):
+        if self.root:
+            return self.root.insert(d)
+        else:
+            self.root = Node(d)
+            return True
+t = Tree()
+print (t.insert(6))
+print (t.insert(2))
+print (t.insert(3))
+print (t.insert(7))
+'''
